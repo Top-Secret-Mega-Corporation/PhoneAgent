27de2a2cd19bc47b71362e155fa6334d878986af
Lucas Webber <lucas@lucaswebber.dev>
2026-02-23 21:42:42 -0500

    Add default mongodb url

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI || "");
+const client = new MongoClient(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/");
 const db = client.db()
 
 export const auth = betterAuth({
